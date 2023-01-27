80b95b5709e893f569a504f63931ab07eee9060b
Sian <cqoute@gmail.com>
2023-01-27 12:23:09 +1030

    Change Zigbee2MQTT legacy from info to warning

--- js/dialogs/dialog-identifier-auto-discovery.ts
+++ js/dialogs/dialog-identifier-auto-discovery.ts
@@ -77,7 +77,7 @@ class SwitchManagerDialogIdentifierAutoDiscovery extends LitElement
                 scrimClickAction
                 @closed=${this.closeDialog}
                 .heading="${createCloseHeading(this.hass, 'Identifier')}">
-                ${this._blueprint!.service == 'Zigbee2MQTT' ? html`<ha-alert alert-type="info">In Zigbee2MQTT ensure your device setting has legacy set to false/off</ha-alert>`: ''}
+                ${this._blueprint!.service == 'Zigbee2MQTT' ? html`<ha-alert alert-type="warning">In Zigbee2MQTT ensure your device setting has legacy set to false/off</ha-alert>`: ''}
                 ${this._blueprint!.info ? html`<ha-alert alert-type="info">${this._blueprint!.info}</ha-alert>`: ''}
                 ${this._error ? html`<ha-alert alert-type="error">${this._error}</ha-alert>` : ""}
                 <ha-textfield 

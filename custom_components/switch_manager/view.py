fc41726241786169d23eb64a21da6ec72195188e
Benjamin Bender <bb@codepoet.de>
2025-03-25 20:12:05 +0100

    chore: revert formatting

--- custom_components/switch_manager/view.py
+++ custom_components/switch_manager/view.py
@@ -5,18 +5,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.components.frontend import async_register_built_in_panel
 from homeassistant.components.http import StaticPathConfig
 
-
 async def async_setup_view(hass: HomeAssistant):
-    staticJS = [
-        StaticPathConfig(
-            PANEL_URL,
-            hass.config.path(
-                "custom_components/switch_manager/assets/switch_manager_panel.js"
-            ),
-            True,
-        )
-    ]
-
+    staticJS = [StaticPathConfig(PANEL_URL, hass.config.path("custom_components/switch_manager/assets/switch_manager_panel.js"), True)]
+    
     # folder = pathlib.Path(hass.config.path("custom_components/switch_manager/assets"))
     # staticJS = [];
     # for f in _find_files(folder, "*.js"):
@@ -27,8 +18,7 @@ async def async_setup_view(hass: HomeAssistant):
     await hass.http.async_register_static_paths(staticJS)
     await async_bind_blueprint_images(hass)
 
-    async_register_built_in_panel(
-        hass,
+    async_register_built_in_panel(hass,
         component_name="custom",
         sidebar_title=NAME,
         sidebar_icon="mdi:light-switch-off",
@@ -38,24 +28,23 @@ async def async_setup_view(hass: HomeAssistant):
             "_panel_custom": {
                 "name": "switch-manager-panel",
                 "module_url": f"{PANEL_URL}?{VERSION}",
-                "embed_iframe": True,
+                "embed_iframe": True
             },
-            "version": VERSION,
+            "version": VERSION
         },
     )
 
-
 async def async_bind_blueprint_images(hass: HomeAssistant):
     static_paths = []
-
+    
     for key in hass.data[DOMAIN].get(CONF_BLUEPRINTS):
         if hass.data[DOMAIN].get(CONF_BLUEPRINTS)[key].has_image:
             static_paths.append(
                 StaticPathConfig(
-                    f"/assets/{DOMAIN}/{key}.png",
+                    f'/assets/{DOMAIN}/{key}.png',
                     hass.config.path(f"{BLUEPRINTS_FOLDER}/{DOMAIN}/{key}.png"),
-                    True,
+                    True
                 )
             )
-
+            
     await hass.http.async_register_static_paths(static_paths)
